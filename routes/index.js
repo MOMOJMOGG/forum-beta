d50a3059a47352e49fda2154834910b973b8bad6
MOMOJMOGG <momomacaca@gmail.com>
2021-10-26 00:01:25 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-// 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
-// 新增
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
+router.use('/admin', admin) // 新增這行
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 module.exports = router
