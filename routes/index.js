357b4499633f37a362ab50d99aafaa53df9f2536
MOMOJMOGG <momomacaca@gmail.com>
2021-10-27 23:12:44 +0800

    feat: flash msg & singup verification

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
-const userController = require('../controllers/user-controller') // 新增這行
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler') // 加入這行
 router.use('/admin', admin)
-// 新增下面這兩行，注意順序
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler) // 加入這行
 module.exports = router
