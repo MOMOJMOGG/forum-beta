3bd31bf2118439e75874e37480f9c36ab359f55b
MOMOJMOGG <momomacaca@gmail.com>
2021-10-28 23:14:06 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const restController = require('../controllers/restaurant-controller')
-const { authenticated } = require('../middleware/auth') // 引入 auth.js
+const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 修改這一行
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin) // 修改這一行
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 Post
 router.get('/logout', userController.logout)
-router.get('/restaurants', authenticated, restController.getRestaurants) // 修改這行，新增 authenticated 參數
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 module.exports = router
