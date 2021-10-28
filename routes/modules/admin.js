f04a516013ea46df82769338d9dc58a2ea2132b1
MOMOJMOGG <momomacaca@gmail.com>
2021-10-28 23:23:04 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-router.get('/restaurants/create', adminController.createRestaurant) // 刪除 authenticatedAdmin
-router.get('/restaurants', adminController.getRestaurants) // 刪除 authenticatedAdmin
-router.post('/restaurants', adminController.postRestaurant) // 新增這行
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant) // 新增這一行
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
