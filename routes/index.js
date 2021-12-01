911273c0335d389e0be81eeb0a5175644b10445a
MOMOJMOGG <momomacaca@gmail.com>
2021-12-01 23:50:14 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,17 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const restController = require('../controllers/restaurant-controller')
-const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 修改這一行
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
-router.use('/admin', authenticatedAdmin, admin) // 修改這一行
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 Post
 router.get('/logout', userController.logout)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant) // 新增這行
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
