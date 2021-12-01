c87edd91c899489ff76399b862fab315f194297a
MOMOJMOGG <momomacaca@gmail.com>
2021-12-01 23:22:06 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -12,6 +12,8 @@ router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/users', adminController.getUser)
 router.patch('/users/:id', adminController.patchUser)
+router.get('/categories/:id', categoryController.getCategories) // 新增這行
+router.put('/categories/:id', categoryController.putCategory) // 新增這行
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
